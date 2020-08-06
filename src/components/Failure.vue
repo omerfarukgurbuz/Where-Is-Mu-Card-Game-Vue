67fb5f837b632651b80d50ca03fba5510fa857ee
Ömer Faruk Gürbüz <omer.gurbuz@kletech.com>
2020-08-06 15:53:34 +0300

    #11 Failure componenti duzenlendi

--- src/components/Failure.vue
+++ src/components/Failure.vue
@@ -1,13 +1,52 @@
 <template>
-  <h3>Failure</h3>
+  <div class="container">
+    <h1>Ne yazıh ki bilemediniz</h1>
+    <button @click="newGame">Yeni Oyun</button>
+  </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    newGame(){
 
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  .container{
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: darkred ;
+  }
+  .container h1{
+    text-align: center;
+    font-size: 333%;
+  }
+  .container button{
+    cursor: pointer;
+    width: 200px;
+    background-color: #fff;
+    color: darkred;
+    border: 1px solid brown;
+    height: 50px;
+    font-size: 15px;
+    border-radius: 5px;
+    outline: none;
+    transition: all .5s;
+  }
+  .container button:hover{
+    background-color: lightgreen;
+    color: #fff;
+    transition: all .5s;
+  }
 </style>
