a0f2fc428e018a0e84eb3201a5639546d329ffee
Ömer Faruk Gürbüz <omer.gurbuz@kletech.com>
2020-08-06 15:59:39 +0300

    #12 Celebrate componenti duzenlendi css eklendi

--- src/components/Celebrate.vue
+++ src/components/Celebrate.vue
@@ -1,14 +1,53 @@
 <template>
-  <h3>Celebrate</h3>
-
+  <div class="container">
+    <h1>Tebrihlerr bildinizz</h1>
+    <button @click="newGame">Yeni Oyun</button>
+  </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    newGame(){
 
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.container{
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.container h1{
+  text-align: center;
+  font-size: 333%;
+  color: darkgreen;
+  opacity: .5;
+}
+.container button{
+  cursor: pointer;
+  width: 200px;
+  background-color: #fff;
+  color: darkred;
+  border: 1px solid darkgreen;
+  height: 50px;
+  font-size: 15px;
+  border-radius: 5px;
+  outline: none;
+  transition: all .5s;
+}
+.container button:hover{
+  background-color: darkgreen;
+  color: #fff;
+  transition: all .5s;
+}
 </style>
